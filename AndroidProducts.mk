2c333472fea0f5bc7a849c2388aa94e8aae3a9bd
Frost <karanraj132804@gmail.com>
2024-03-28 21:25:44 +0530

    veux: Welcome to VoltageOS
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2023-2024 The LineageOS Project
+#               2023-2024 VoltageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_veux.mk
+    $(LOCAL_DIR)/voltage_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_veux-user \
-    lineage_veux-userdebug \
-    lineage_veux-eng
+    voltage_veux-user \
+    voltage_veux-userdebug \
+    voltage_veux-eng
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2023-2024 The LineageOS Project
+#               2023-2024 VoltageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #

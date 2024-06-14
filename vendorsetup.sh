017426bcc4e759b9d28742a12c708202107e7932
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-06-14 18:22:35 +0530

    veux: Clone own audio config tree
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+# Clone own audio config tree
+rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.com/veux-frost-testzone/hardware_qcom-caf_sm8350_audio_configs_holi.git -b dolby hardware/qcom-caf/sm8350/audio/configs/holi --depth=1
